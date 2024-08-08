27f658444c237f3bc1a511a3a43271904f0642b1
Erick Koome <koomembwiria@gmail.com>
2024-08-08 06:24:13 +0300

    adding customer-dashboard

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import AccountDetails from './components/AccountDetails';
 import Register from './components/Register';
 import SignIn from './components/SignIn';
 import OrganizerDashboard from './components/OrganizerDashboard';
+import CustomerDashboard from './components/CustomerDashboard';
 import ContactForm from './components/ContactForm';
 import RequestOTP from './components/RequestOTP';
 import VerifyOTP from './components/VerifyOTP';
@@ -22,6 +23,7 @@ function App() {
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<SignIn />} />
           <Route path="/organizer-dashboard" element={<OrganizerDashboard />} />
+          <Route path="/customer-dashboard" element={<CustomerDashboard />} />
           <Route path="/contact-us" element={<ContactForm />} />
           <Route path="/request-otp" element={<RequestOTP />} />
           <Route path="/verify-otp" element={<VerifyOTP />} />
